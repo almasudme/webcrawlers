2c54050fa864022310f530659b6c046917cad02c
mir <almasudme@gmail.com>
2024-03-24 21:22:44 -0400

    Changes to be committed: 	modified:   get_hotel_data.py 	modified:   hotels_info.csv 	modified:   info_cities.json

--- hotels/get_hotel_data.py
+++ hotels/get_hotel_data.py
@@ -6,6 +6,7 @@ import pandas as pd
 import time
 import sys
 import os
+import sqlite3
 
 # selenium
 from selenium import webdriver
@@ -15,7 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from fake_useragent import UserAgent
 
 # ==================== API Functions====================
 
@@ -23,6 +24,21 @@ options = Options()
 # options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+
+ua = UserAgent()
+user_agent = ua.random
+print(user_agent)
+
+options.add_argument(f'--user-agent={user_agent}')
+
+# Adding argument to disable the AutomationControlled flag 
+options.add_argument("--disable-blink-features=AutomationControlled") 
+ 
+# Exclude the collection of enable-automation switches 
+options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+ 
+# Turn-off userAutomationExtension 
+options.add_experimental_option("useAutomationExtension", False) 
 
 
 # ======== Selenium Helper Function ========================
@@ -46,50 +62,97 @@ def get_location_dict(text = ''):
 def get_info(url):
     
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    time.sleep(2)
+    time.sleep(1)
     driver.get(url)
     time.sleep(5)
-    _xpath = '//*[@id="HEADING"]'
+    # _xpath = '//*[@id="HEADING"]'
     info_dict = {}
+    review_dict = {}
     info_dict['location_id'] = re.findall(r'_Review-g.+-d(\d+)-',url)[0]
     info_dict['hotel_name'] = driver.find_element(By.XPATH,'//*[@id="HEADING"]').text
     info_dict['phone_number'] = driver.find_element(By.XPATH,'//*[@id="lithium-root"]/main/span/div[4]/div[1]/div/div[3]/div/div[3]/div[1]/div[3]/div[2]/a/div/div').text
     info_dict['faq_text'] = driver.find_element(By.XPATH,'//*[@id="FAQ"]').text
     location_text = driver.find_element(By.XPATH,'//*[@id="LOCATION"]').text
+    review_text = driver.find_element(By.XPATH,'//*[@id="LOCATION"]').text
     
     location_dict = get_location_dict(text = location_text)
     info_dict.update(location_dict)
     driver.close()
-    # driver.quit()
-    
-    # print(json.dump(info_dict,open('output_json.json','w')))
+
+
     return info_dict
 
+def get_review_from_text(input_text):
+    
+    list_input_text = input_text.split("\n")
+    name,time = re.findall('(.+) wrote a review(.+)$',list_input_text[0])[0]
+    contributions,helpful_votes = \
+        re.findall(r'(\d+) contributions(\d+) helpful votes$',list_input_text[1])[0]
+    response_review = ''
+    has_response = False
+    response_date = None
+    
+    post_helpful = 0
+    responded_person = None
+    for text in list_input_text[2:]:
+        if text.startswith('Date of stay:'):
+            date = re.findall(r'Date of stay: (.+)$',text)[0]
+        elif text.startswith('Response from'):
+            responded_person = re.findall(r'Response from (.+)$',text)[0]
+        elif text.startswith('Responded '):
+            response_date = re.findall(r'Responded (.+)$',text)[0]
+            has_response = True
+        elif text.startswith('This response is the subjective opinion'):
+            response_review += f"\n {text}"
+        elif text.startswith('Report response as inappropriate'):
+            response_review += f"\n {text}"
+        elif has_response and (not text.startswith('Read more')):
+            response_review += text
+        elif re.findall(r'^(\d+)$',text):
+            post_helpful = re.findall(r'^(\d+)$',text)[0]
+                    
+    return_dict = {
+        'date':date,
+        'name':name,
+        'time': time,
+        'helpful_votes': float(helpful_votes),
+        'contributions': contributions,
+        'responded_person': responded_person,
+        'response_date':response_date,
+        'response_review':response_review,
+        'post_helpful':post_helpful
+        
+        
+        }
+    return return_dict
+
+    
 
 # ======== Selenium Helper Function ========================
-def get_reviews(url ):
-    for page in  range(2):
-        if page == 0:        
-            _xpath = f'//*[@id="lithium-root"]/main/span/div[4]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]/'
-        else:
-            # url = f"https://www.tripadvisor.com/Hotel_Review-g187147-d230431-Reviews-or{page}0-Hotel_Astoria_Astotel-Paris_Ile_de_France.html"
-            url = url.replace('-Reviews-',f'-Reviews-or{page}0-')
-            _xpath = f'//*[@id="lithium-root"]/main/span/div[3]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]/' 
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)            
-        driver.get(url)
-        time.sleep(5)
-        for idiv in range(1,3):
-            xpath = _xpath + f'div[{idiv}]'
+# def update_review_csv(url ):
+#     for page in  range(2):
+#         if page == 0:        
+#             _xpath = f'//*[@id="lithium-root"]/main/span/div[4]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]/'
+#         else:
+#             # url = f"https://www.tripadvisor.com/Hotel_Review-g187147-d230431-Reviews-or{page}0-Hotel_Astoria_Astotel-Paris_Ile_de_France.html"
+#             url = url.replace('-Reviews-',f'-Reviews-or{page}0-')
+#             _xpath = f'//*[@id="lithium-root"]/main/span/div[3]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]/' 
+#         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)            
+#         driver.get(url)
+#         time.sleep(5)
+#         for idiv in range(1,3):
+#             xpath = _xpath + f'div[{idiv}]'
             
-            print(f'xpath: {xpath}')
-            print(f'=========Page: {page+1}, Review no: {idiv}============')
-            print(f'{xpath}')
-            xpath_element = driver.find_element(By.XPATH,xpath)
-            time.sleep(1)
-            print(xpath_element.text)
-            dict_hotel_reviews = {}
-        driver.close()
-        driver.quit()
+            
+#             print(f'xpath: {xpath}')
+#             print(f'=========Page: {page+1}, Review no: {idiv}============')
+#             print(f'{xpath}')
+#             xpath_element = driver.find_element(By.XPATH,xpath)
+#             time.sleep(1)
+#             print(xpath_element.text)
+#             dict_hotel_reviews = {}
+#         driver.close()
+#         driver.quit()
 
 
 
@@ -168,39 +231,7 @@ def get_locations_ids(filename = 'cities.txt'):
      
     return cities
 
-
-if __name__ == "__main__":
-    # Replace with your actual API key
-    my_api_key = "95175A870CB7432FA39DF1715B267E6D"
-    cities = get_cities(filename = 'cities.txt')
-    # location_ids= get_location_ids()
-    new_cities = []
-    bar_length = 48
-    count = 0
-    # for i,c_s in enumerate(cities[120:130]):
-    #     # print(c_s)
-    #     time.sleep(0.02)
-    #     temp_info_cities = str(get_tripadvisor_hotels(api_key=my_api_key, city_state = c_s))
-    #     temp_info_cities = eval(temp_info_cities)
-    #     new_cities.extend(temp_info_cities)
-    #     sys.stdout.write('\r')
-    #     sys.stdout.write(f"Completed: {c_s} |[{i} of {20}]")
-    
-    # review_df.to_sql(con=conn,index=True,if_exists="replace",name="info")
-    # conn.close()
-
-    with open ("info_cities.json",'r') as f:
-        text= f.read()
-    dict_info_cities= eval(text)
-    # dict_info_cities.extend(new_cities)
-    # json.dump(dict_info_cities,open('info_cities.json','w'))
-    df = pd.DataFrame.from_dict(dict_info_cities)
-    df.to_csv('blabla.csv')
-    
-    db_file_name = "hotels_data_1.db"
-
-    import sqlite3
-    conn = sqlite3.connect(db_file_name)
+def update_info_csv(csv_name = 'hotels_info.csv'):    
     print(f"hotel count: {len(dict_info_cities)}")
     list_of_dicts = []
     count = 0
@@ -209,15 +240,11 @@ if __name__ == "__main__":
     total = len(dict_info_cities)
     
     pre_existing = dict()
-    if os.path.exists('blabla_new.csv'):
-        pre_csv_df = pd.read_csv('blabla_new.csv')
-        for x in pre_csv_df['location_id']:
-
-
-
-
-            
+    if os.path.exists(csv_name):
+        pre_csv_df = pd.read_csv(csv_name)
+        for x in pre_csv_df['location_id']:            
             pre_existing[str(x)] = True
+
     for dict_city in dict_info_cities:
         count += 1
         time.sleep(2)
@@ -262,14 +289,181 @@ if __name__ == "__main__":
 
     
     temp_df = pd.DataFrame.from_dict(list_of_dicts)
-    if os.path.exists('blabla_new.csv'):
+    if os.path.exists(csv_name):
         df = pd.concat([pre_csv_df,temp_df])
     else:
         df = temp_df
     
     df = df[['hotel_name', 'rating', 'n_reviews', 'address', 'ranking','about_text', 'amenities_list', 'contact', 'web_url', 'location_id', 'phone_number', 'faq_text', 'walking_grade','nearby_restaurants_summary', 'nearby_attractions_summary','getting there', 'nearby_restaurants', 'nearby_attractions']]
-    df.to_csv('blabla_new.csv')
+    df.to_csv(csv_name)
     # # df.to_sql(con=conn,index=True,if_exists="replace",name="info")
     # # conn.close()
     # print(f'succeeded hotels {succeeded_hotel}')
     print(f'succeeded {len(succeeded_hotel)}/{len(dict_info_cities)}')
+
+def set_viewport_size(driver, width, height):
+    window_size = driver.execute_script("""
+        return [window.outerWidth - window.innerWidth + arguments[0],
+          window.outerHeight - window.innerHeight + arguments[1]];
+        """, width, height)
+    driver.set_window_size(*window_size)
+def sqlite_update(data,table):
+    db_file_name = "hotels_data_1.db"
+    conn = sqlite3.connect(db_file_name)
+    cursor = conn.cursor()
+    insert_str = f"INSERT INTO reviews {str(tuple(data.keys()))} VALUES {str(tuple(data.values()))}"
+    print(insert_str)
+    cursor.execute(insert_str)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    
+    
+    
+
+def update_review_csv(csv_name = 'hotel_reviews.csv'):
+    print(f"hotel count: {len(dict_info_cities)}")
+    list_of_dicts = []
+    count = 0
+    succeeded_hotel=[]
+    failed_hotel=[]
+    total = len(dict_info_cities)
+    
+    pre_existing = dict()
+    if os.path.exists(csv_name):
+        pre_csv_df = pd.read_csv(csv_name)
+        for x in pre_csv_df['location_id']:            
+            pre_existing[str(x)] = True
+    for dict_city in dict_info_cities:
+        count += 1
+        time.sleep(1)
+        location_id = dict_city.get('location_id')
+        # print(location_id,type(location_id),pre_existing)
+        if pre_existing.get(location_id,False):
+            print(f"{count}. Item exists {location_id}")
+            continue
+        review_dict = {}
+        hotel_name = dict_city.get('name')
+        hotel_url_0 =  dict_city.get('web_url').replace('?m=66827','')
+        for page in range(4):
+            if page == 0: 
+                hotel_url = hotel_url_0       
+                _xpath = f'//*[@id="lithium-root"]/main/span/div[4]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]'
+            else:
+                hotel_url = hotel_url_0.replace('-Reviews-',f'-Reviews-or{page}0-')
+                _xpath = f'//*[@id="lithium-root"]/main/span/div[3]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]' 
+                                                    
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)            
+            time.sleep(0.20)
+            set_viewport_size(driver, 800, 600)
+            time.sleep(0.25)
+            # Changing the property of the navigator value for webdriver to undefined 
+            driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+            time.sleep(0.23)
+            try:
+                driver.get(hotel_url)
+                
+                
+                
+                
+            except:
+                print(f"Could not get url {hotel_url}")
+                driver.close()
+                driver.quit()
+                continue
+            # finally:
+            #     sys.stdout.write('\r')
+            #     sys.stdout.write(f"Completed: [{count} of {total}]")
+            #     sys.stdout.flush()
+            time.sleep(5)
+            # Iterate over this page's reviews
+            for idiv in range(1,12):
+                xpath = _xpath + f'/div[{idiv}]'
+                try:
+                    full_text = driver.find_element(By.XPATH,xpath).text
+                
+                    print(full_text)
+                    temp = get_review_from_text(full_text)
+                    
+                    print(f'xpath: {xpath}')
+                    print(f'=========Page: {page+1}, Review no: {idiv}============')
+                    print(f'{xpath}')
+                    review_id = f"{location_id}_{page+1}_{idiv}"
+                    time.sleep(0.15)
+                    # date = driver.find_element(By.XPATH,xpath + f'/div[2]/div[3]/div/span[@class="iSNGb _R Me S4 H3 Cj"]').text
+                    # time.sleep(0.10)
+                    # name = driver.find_element(By.XPATH,xpath + f'/div[1]/div/div[2]/div/div/span/a').text
+                    time.sleep(0.12)
+                    review_title = driver.find_element(By.XPATH,xpath + f'/div[2]/div[2]').text
+                    time.sleep(0.17)
+                    review = driver.find_element(By.XPATH,xpath + f'/div[2]/div[3]/div[1]/div[1]').text
+                    time.sleep(0.15)
+                    response_review = "TBD"#driver.find_element(By.XPATH,'//*[@id="lithium-root"]/main/span/div[3]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div[1]/div[1]/div[2]/div[3]/div[4] ')
+                    # post_helpful = driver.find_element(By.XPATH,'//*[@id="lithium-root"]/main/span/div[3]/div[3]/div/div[1]/div/div/div[1]/div[3]/div[3]/div/div[1]/div[2]/div[3]/div[3]/button[@class="BrOJk u j z _F wSSLS HuPlH Vonfv"]/span[@class="kLqdM"]/span[@class="biGQs _P FwFXZ"]').text
+                    dict_hotel_reviews = {
+
+                        'hotel_name' : hotel_name,
+                        'rating': dict_city.get('rating'),
+                        # 'trip_type':driver.find_element(By.XPATH,xpath + f'/div[2]/div[3]/div/span[@class="hHMDb _R Me"]').text,
+                        'review_title': review_title,
+                        'review':review,
+                        }
+                    dict_hotel_reviews.update(temp)
+                    pprint(dict_hotel_reviews)
+                    time.sleep(1)
+                    sqlite_update(data=dict_hotel_reviews,table='reviews')
+                except:
+                    print("could not find {hotel_url}")
+
+            driver.close()
+            driver.quit()                
+            # break
+        # break
+        
+#==============================================================
+def update_info_cities_json(input_file='cities.txt',output_file='info_cities.json'):
+    my_api_key = "95175A870CB7432FA39DF1715B267E6D"
+    cities = get_cities(filename = input_file)
+    # location_ids= get_location_ids()
+    new_cities = []
+    bar_length = 48
+    count = 0
+    for i,c_s in enumerate(cities[301:400]):
+        # print(c_s)
+        time.sleep(0.02)
+        temp_info_cities = str(get_tripadvisor_hotels(api_key=my_api_key, city_state = c_s))
+        temp_info_cities = eval(temp_info_cities)
+        new_cities.extend(temp_info_cities)
+        sys.stdout.write('\r')
+        sys.stdout.write(f"Completed: {c_s} |[{i} of {20}]")
+    
+    # review_df.to_sql(con=conn,index=True,if_exists="replace",name="info")
+    # conn.close()
+
+    with open (output_file,'r') as f:
+        text= f.read()
+    dict_info_cities= eval(text)
+    dict_info_cities.extend(new_cities)
+    json.dump(dict_info_cities,open(output_file,'w'))
+    exit()
+
+def get_dict_from_json(json_file = 'info_cities.json'):
+    with open (json_file,'r') as f:
+        text= f.read()
+    return eval(text)
+
+#==============================================================
+if __name__ == "__main__":
+    # Replace with your actual API key
+    
+    ### Create input json file 
+    # update_info_cities_json()
+    dict_info_cities = get_dict_from_json(json_file = 'info_cities.json')
+    # df = pd.DataFrame.from_dict(dict_info_cities)
+    # df.to_csv('blabla.csv')
+
+    ### Create hotel info csv 
+    # update_info_csv('hotels_info.csv')
+    # ## Create hotel reviews csv 
+    update_review_csv('hotel_reviews.csv')
